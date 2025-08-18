577cd5cff947fb09ce74201545c0d6631f5be251
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-08-18 12:24:18 -0300

    Project created

---
+++ src/test/java/com/fabioalves/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabioalves.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
