5f742bfdcf8a2310206b47d6c57045ead766bf9c
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-08-19 12:44:06 -0300

    Implemented domain model

---
+++ src/main/java/com/fabioalves/dslist/entities/Belonging.java
@@ -0,0 +1,30 @@
+package com.fabioalves.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_belonging")
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer position;
+
+    public Belonging (Game game, GameList gameList, Integer position) {
+        id.setGame(game);
+        id.setGameList(gameList);
+        this.position = position;
+    }
+}
---
+++ src/main/java/com/fabioalves/dslist/entities/BelongingPK.java
@@ -0,0 +1,27 @@
+package com.fabioalves.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+}
